93dec42b1b2588f74d070bed77801a0c76c9eb1d
venkateshkumarmandla <venkateshkumar.mandla@tekworks.in>
2025-06-19 19:20:19 +0530

    ui changes for video and logic

--- src/services/ApplicantAPIService.js
+++ src/services/ApplicantAPIService.js
@@ -5,11 +5,15 @@
 
 
  
-                export const apiUrl = 'http://192.168.200.101:8081';
-                // export const apiUrl = 'http://192.168.1.60:8081';
+            //     export const apiUrl = 'http://192.168.200.101:8081';
+          
+                            export const apiUrl = 'http://192.168.1.60:8081';
 
+                        //     export const apiUrl = 'http://192.168.137.1:8081';
 
-            // export const apiUrl = 'http://192.168.137.1:8081';
+            
+ 
+ 
 
 
 
@@ -18,7 +22,7 @@
 // 192.168.1.13
       // export const apiUrl = 'https://kqryamxpv3.ap-south-1.awsapprunner.com';
 
-    //  export const apiUrl = 'https://g23jza8mtp.ap-south-1.awsapprunner.com';
+//      export const apiUrl = 'https://g23jza8mtp.ap-south-1.awsapprunner.com';
 
 
           //  export const apiUrl = 'http://192.168.1.51:8081';
